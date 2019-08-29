d5f78e3bcf03bbf29e33e9fc8f885462df14d578
SiAsp <sindre.asplem@gmail.com>
2019-08-29 13:55:54 +0200

    Working state, generates pic with quote and tweets.

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 # @Inspira39021145 <---- Twitter handle
-import tweepy # Twitter module
+import tweepy  # Twitter module
 
-# Sånne greier vi trenger for autentikasjon
-api_key, api_secret = "WO1t9lUsG8OjAoEnJrU37Ohzd", "31YhGtvSEadWkN5uL5FEdFpd4ddW4VtcaIW4sKJ92bkStBuAX2"
-token_key, token_secret = "1166347129025155072-EaGxHUB9bUf5ImDmdyXOXPq4U5cZSZ", "bbJtob2GjLx4of2NwTYjMkw7OdBhhFzimufoFzySPFzKd"
 
-# Konstruksjon av et objekt vi kan bruke. bla bla bla--
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(token_key, token_secret)
-api = tweepy.API(auth)
+def main():
+    # Sånne greier vi trenger for autentikasjon
+    api_key, api_secret = "WO1t9lUsG8OjAoEnJrU37Ohzd", "31YhGtvSEadWkN5uL5FEdFpd4ddW4VtcaIW4sKJ92bkStBuAX2"
+    token_key, token_secret = "1166347129025155072-EaGxHUB9bUf5ImDmdyXOXPq4U5cZSZ", "bbJtob2GjLx4of2NwTYjMkw7OdBhhFzimufoFzySPFzKd"
 
-# FAKTISK BRUK AV API
-api.update_status("Tweet") # Dette er legit ossn man tweeter.
+    # Konstruksjon av et objekt vi kan bruke. bla bla bla--
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(token_key, token_secret)
+    api = tweepy.API(auth)
+
+    # FAKTISK BRUK AV API
+    # api.update_status("Tweet") # Dette er legit ossn man tweeter.
+    api.update_with_media('photo_of_the_day.png')
+
+
+if __name__ == '__main__':
+    main()
--- run.py
+++ run.py
@@ -1,22 +1,51 @@
-def main():
-    put_quote_on_wallpaper("maxresdefault.jpg", "Kermit the frog did 9/11")
+import random
+import requests
+import urllib
+from PIL import Image, ImageFont, ImageDraw
+import bot
+
+
+def select_quote():
+    with open('bible_verses.txt') as bible:
+        bible_list = bible.read().split('\n\n')
+        # ----- Limit length of quote -----#
+        while True:
+            verse = random.choice(bible_list)
+            if len(verse) > 110:
+                verse = random.choice(bible_list)
+            else:
+                break
+        verse = verse[verse.find(' ') + 1:].rstrip()
+        return verse
+
+
+def get_img():
+    pic_num = random.randint(2, 1084)
+    url = 'https://picsum.photos/800/1200'
+    #r = requests.get(url)
+    # print(r.text)
+    urllib.request.urlretrieve(url, './photo_of_the_day.png')
+
 
 def put_quote_on_wallpaper(wallpaper, biblequote):
-    from PIL import Image, ImageFont, ImageDraw
-
-# Open image --------------------------------------- #
+    # ----- Open image -----#
     image = Image.open(wallpaper)
-
-# Select Font-type, Font-size ---------------------- #
-    font = ImageFont.truetype("comic.ttf", 48)
-
-# Draw text onto wallpaper ------------------------- #
+# ----- Select Font-type, Font-size ----- #
+    font = ImageFont.truetype("/Library/Fonts/arial.ttf", 24)
+# ----- Draw text onto wallpaper ----- #
     draw = ImageDraw.Draw(image)
     draw.text((100, 100), biblequote, "black", font)
+    print(123)
+    image.save('./photo_of_the_day.png')
 
-# Debug
-    image.save("1.png")
+
+def main():
+    verse = select_quote()
+    get_img()  # Download img
+    put_quote_on_wallpaper('./photo_of_the_day.png', verse)
+    # ---- Upload img to twitter ---- #
+    bot.main()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
