3cbe7409b54be2456986264c635007f3e12599ce
Eruerufusama <34217008+Eruerufusama@users.noreply.github.com>
2019-08-29 11:33:40 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+# @Inspira39021145
+import tweepy
+
+api_key = "WO1t9lUsG8OjAoEnJrU37Ohzd"
+api_secret = "31YhGtvSEadWkN5uL5FEdFpd4ddW4VtcaIW4sKJ92bkStBuAX2"
+
+token_key = "1166347129025155072-EaGxHUB9bUf5ImDmdyXOXPq4U5cZSZ"
+token_secret = "bbJtob2GjLx4of2NwTYjMkw7OdBhhFzimufoFzySPFzKd"
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(token_key, token_secret)
+api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+
+# Dette greiene var bare noe testgreier. Ignorer.
+for mention in mentions:
+    
+    api.update_status("@" + mention.user.screen_name + " Sup dude.", mention.id)
