a7a767d4a8cdb82ea7e96873e4d850a2025bcce3
SiAsp <sindre.asplem@gmail.com>
2019-08-29 13:21:13 +0200

    Working state, generates pic with quote

--- bible_app.py
+++ bible_app.py
@@ -3,6 +3,7 @@
 
 
 '''
+https://picsum.photos/id/1084/800/1200
 
 tokens.py
 
@@ -21,14 +22,47 @@ tokens.py
 
 import random
 import requests
+import urllib
+from PIL import Image, ImageFont, ImageDraw
 
 
 def select_quote():
     with open('bible_verses.txt') as bible:
-        b = bible.read().split('\n\n')
-        verse = random.choice(b)
-        verse = verse[verse.find(' ') + 1:]
-        print(verse)
+        bible_list = bible.read().split('\n\n')
+        # ----- Limit length of quote -----#
+        while True:
+            verse = random.choice(bible_list)
+            if len(verse) > 110:
+                verse = random.choice(bible_list)
+            else:
+                break
+        verse = verse[verse.find(' ') + 1:].rstrip()
+        return verse
 
 
-select_quote()
+def get_img():
+    pic_num = random.randint(2, 1084)
+    url = 'https://picsum.photos/800/1200'
+    #r = requests.get(url)
+    # print(r.text)
+    urllib.request.urlretrieve(url, './photo_of_the_day.jpeg')
+
+
+def put_quote_on_wallpaper():
+    # ----- Open image -----#
+    get_img()
+    image = Image.open("photo_of_the_day.jpeg")
+# ----- Create string -----#
+    string = select_quote()
+# ----- Select Font-type, Font-size ----- #
+    font = ImageFont.truetype("/Library/Fonts/arial.ttf", 24)
+# ----- Draw text onto wallpaper ----- #
+    draw = ImageDraw.Draw(image)
+    draw.text((100, 100), string, "black", font)
+
+    # Debug
+    image.show()
+
+
+if __name__ == '__main__':
+    put_quote_on_wallpaper()
