8759916b1181bd68cf018927d34b91058d2d107b
Eruerufusenpai <martin.faeroy@gmail.com>
2019-08-31 05:41:42 +0200

    retracted bugfixes. They fixed nothing.

--- run.py
+++ run.py
@@ -55,15 +55,7 @@ def select_quote():
 def get_img():
     pic_num = random.randint(2, 1084)
     url = 'https://picsum.photos/1080/1920'
-
-# TEMPORALY FIX ( High Priority )
-    while True:
-        try:
-            urllib.request.urlretrieve(url, './photo_of_the_day.jpg')
-            break
-        except:
-            print("An error occured while attempting to load image")
-            sleep(1)
+    urllib.request.urlretrieve(url, './photo_of_the_day.jpg')
 
 
 def put_quote_on_wallpaper(wallpaper, biblequote):
@@ -74,16 +66,7 @@ def put_quote_on_wallpaper(wallpaper, biblequote):
         font = ImageFont.truetype("fonts/truetype/arial.ttf", 48)
 
 # Open layers
-
-# TEMPORALY FIX ( High Priority )
-    while True:
-        try:
-            image = Image.open(wallpaper)  # Background
-            break
-        except OSError:
-            print("An error occured when attempting to open image")
-            sleep(1)
-
+    image = Image.open(wallpaper)  # Background
     text_layer = Image.new('RGBA', (image.size[0], image.size[1]), None)  # Text-layer
 
 # Create draw-object
