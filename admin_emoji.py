bec1b55b7390eae96bd311a51b608a049cd39054
SiAsp <sindre.asplem@gmail.com>
2019-09-17 11:29:07 +0200

    Fully implement log, no printstatements only logging to log.txt

--- admin_emoji.py
+++ admin_emoji.py
@@ -4,44 +4,44 @@ from sys import argv
 
 
 def create_probability_distribution(li):
-    li_multiplyer = 100 / sum(li)
-    li_out = []
-    for i in li:
-        li_out.append((i * li_multiplyer) / 100)
-    return li_out
+  li_multiplyer = 100 / sum(li)
+  li_out = []
+  for i in li:
+    li_out.append((i * li_multiplyer) / 100)
+  return li_out
 
 
 def append_emoji(dct, elem, val):
-    i_li = [val]
-    i_dict = {elem: val}
-    percentage_dict = {}
+  i_li = [val]
+  i_dict = {elem: val}
+  percentage_dict = {}
 
-    for key, val in dct.items():
-        i_li.append(val * (len(dct)))
+  for key, val in dct.items():
+    i_li.append(val * (len(dct)))
 
-    percentage_li = create_probability_distribution(i_li)
+  percentage_li = create_probability_distribution(i_li)
 
-    percentage_dict[elem] = percentage_li[0]
-    for i, (key, val) in enumerate(dct.items()):
-        i += 1
-        percentage_dict['0001' + key] = percentage_li[i]
+  percentage_dict[elem] = percentage_li[0]
+  for i, (key, val) in enumerate(dct.items()):
+    i += 1
+    percentage_dict['0001' + key] = percentage_li[i]
 
-    return percentage_dict
+  return percentage_dict
 
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        json_file = sys.path[0] + '/resources/emojis.json'
-        dict_emoji = json_to_dict(json_file)
-        try:
-            if argv[1] == 'add':
-                emoji = argv[2]
-                value = int(argv[3])
-                dct = append_emoji(dict_emoji, emoji, value)
-            elif argv[1] == 'rm':
-                del dict_emoji[argv[2]]
-                dct = dict_emoji
+  if len(argv) > 1:
+    json_file = sys.path[0] + '/resources/emojis.json'
+    dict_emoji = json_to_dict(json_file)
+    try:
+      if argv[1] == 'add':
+        emoji = argv[2]
+        value = int(argv[3])
+        dct = append_emoji(dict_emoji, emoji, value)
+      elif argv[1] == 'rm':
+        del dict_emoji[argv[2]]
+        dct = dict_emoji
 
-            write_to_json(json_file, dct)
-        except:
-            print('Unknown parameter. Format: admin_emoji.py \{emoji} \{value}')
+      write_to_json(json_file, dct)
+    except:
+      print('Unknown parameter. Format: admin_emoji.py \{emoji} \{value}')
--- tweet_processing.py
+++ tweet_processing.py
@@ -5,49 +5,48 @@ from pre_processing import json_to_dict
 
 
 def create_header():
-    header, index = select_header()
-    return fill_header_with_emojis(header, 6), index
+  header, index = select_header()
+  return fill_header_with_emojis(header, 6), index
 
 
 def select_header():
-    with open(sys.path[0] + '/resources/tweet_headers.txt') as header:
-        headers = header.read().split("\n\n")
-        index = randint(0, len(headers) - 1)
-        return headers[index], index
+  with open(sys.path[0] + '/resources/tweet_headers.txt') as header:
+    headers = header.read().split("\n\n")
+    index = randint(0, len(headers) - 1)
+    return headers[index], index
 
 
 def fill_header_with_emojis(header, meme_amplitude):
 
-    emoji_dict = json_to_dict(sys.path[0] + '/resources/emojis.json')
-    split_header = header.split()
+  emoji_dict = json_to_dict(sys.path[0] + '/resources/emojis.json')
+  split_header = header.split()
 
-    max_chars = 240
-    # Remaining number of characters allowed in a tweet
-    remaining_chars = max_chars - len(''.join(split_header)) - (meme_amplitude * 2)
-    # Average emojis per word in tweet
-    print(remaining_chars)
-    avg = remaining_chars / (len(split_header))
+  max_chars = 240
+  # Remaining number of characters allowed in a tweet
+  remaining_chars = max_chars - len(''.join(split_header)) - (meme_amplitude * 2)
+  # Average emojis per word in tweet
+  avg = remaining_chars / (len(split_header))
 
-    emoji_keys = list(emoji_dict.keys())
-    emoji_values = list(emoji_dict.values())
-    premojis = ''.join(numpy.choice(emoji_keys, size=meme_amplitude, p=emoji_values))
-    postmojis = ''.join(numpy.choice(emoji_keys, size=meme_amplitude, p=emoji_values))
-    emojified_message = ''
+  emoji_keys = list(emoji_dict.keys())
+  emoji_values = list(emoji_dict.values())
+  premojis = ''.join(numpy.choice(emoji_keys, size=meme_amplitude, p=emoji_values))
+  postmojis = ''.join(numpy.choice(emoji_keys, size=meme_amplitude, p=emoji_values))
+  emojified_message = ''
 
-    # If tweet is to long to add emojis between words
-    if avg < 1:
-        returnstring = premojis + header + postmojis
+  # If tweet is to long to add emojis between words
+  if avg < 1:
+    returnstring = premojis + header + postmojis
 
-    else:
-        for i, word in enumerate(split_header, 1):
-            emojified_message += word
-            # Don't add emojis behind last word, postmojis covers this.
-            if i == len(split_header):
-                break
-            # Insert random number of emojis if average is less than 5
-            for i in range(randint(1, int(avg) if avg < 5 else 5)):
-                # Replace with function returning emojis relative to probability
-                emojified_message += ''.join(numpy.choice(emoji_keys, size=1, p=emoji_values))
+  else:
+    for i, word in enumerate(split_header, 1):
+      emojified_message += word
+      # Don't add emojis behind last word, postmojis covers this.
+      if i == len(split_header):
+        break
+      # Insert random number of emojis if average is less than 5
+      for i in range(randint(1, int(avg) if avg < 5 else 5)):
+        # Replace with function returning emojis relative to probability
+        emojified_message += ''.join(numpy.choice(emoji_keys, size=1, p=emoji_values))
 
-    returnstring = premojis + emojified_message + postmojis
-    return returnstring
+  returnstring = premojis + emojified_message + postmojis
+  return returnstring
